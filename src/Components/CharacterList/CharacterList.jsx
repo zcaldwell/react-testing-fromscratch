645aadf2f8b081dde0a8f1dc6d79ac14abaf851d
zcaldwell <mintyst.john@gmail.com>
2022-01-28 15:26:05 -0800

    some css done

--- src/Components/CharacterList/CharacterList.jsx
+++ src/Components/CharacterList/CharacterList.jsx
@@ -5,37 +5,66 @@ import { useState } from 'react';
 
 export default function CharacterList({ characters }) {
   const [order, setOrder] = useState('N/A');
+  const [query, setQuery] = useState('');
+  const [family, setFamily] = useState('');
 
   function filterCharacters() {
-    return characters.sort((a, b) => {
-      if (order === 'asc') {
-        if (a.firstName < b.firstName) {
-          return -1;
+    return characters
+      .sort((a, b) => {
+        if (order === 'asc') {
+          if (a.firstName < b.firstName) {
+            return -1;
+          }
+          if (a.firstName > b.firstName) {
+            return 1;
+          }
+          return 0;
+        } else if (order === 'dsc') {
+          if (a.firstName > b.firstName) {
+            return -1;
+          }
+          if (a.firstName < b.firstName) {
+            return 1;
+          }
+          return 0;
         }
-        if (a.firstName > b.firstName) {
-          return 1;
-        }
-        return 0;
-      } else if (order === 'dsc') {
-        if (a.firstName > b.firstName) {
-          return -1;
-        }
-        if (a.firstName < b.firstName) {
-          return 1;
-        }
-        return 0;
-      }
-    });
+      })
+      .filter((character) => {
+        return character.fullName.includes(query);
+      })
+      .filter((character) => {
+        return character.family.includes(family);
+      });
   }
 
   return (
     <>
       <div>
-        <select value={order} onChange={(e) => setOrder(e.target.value)}>
-          <option value="n/a">N/A</option>
-          <option value="asc">Ascending</option>
-          <option value="dsc">Descending</option>
-        </select>
+        <div className="search-input">
+          <input
+            placeholder="Search"
+            type="text"
+            value={query}
+            onChange={(e) => {
+              setQuery(e.target.value);
+            }}
+          />
+        </div>
+        <div>
+          <select value={order} onChange={(e) => setOrder(e.target.value)}>
+            <option value="n/a">N/A</option>
+            <option value="asc">Ascending</option>
+            <option value="dsc">Descending</option>
+          </select>
+        </div>
+        <div>
+          <select value={family} onChange={(e) => setFamily(e.target.value)}>
+            <option value="">All</option>
+            <option value="Stark">Stark</option>
+            <option value="Bolton">Bolton</option>
+            <option value="Free Folk">Free Folk</option>
+          </select>
+        </div>
       </div>
       <div>
         <ul className="characterList">
--- src/Views/Home.jsx
+++ src/Views/Home.jsx
@@ -5,7 +5,6 @@ import { getCharacters } from '../services/characters';
 
 export default function Home() {
   const [characters, setCharacters] = useState([]);
-
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
